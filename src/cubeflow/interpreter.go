4d131654f8ad867cb75a26c224dc716563111287
Daker Fernandes Pinheiro <dfp@cin.ufpe.br>
2016-06-23 23:40:41 -0300

    Adds interpreter cmd

--- src/cubeflow/interpreter.go
+++ src/cubeflow/interpreter.go
@@ -6,17 +6,23 @@ import (
 
 func (cell Cell) Clock(halt chan int) {
 	for cell.Type.Exec(cell.Notify, halt) {
-		log.Println("spin", cell)
+		if *debug {
+			log.Println("spin", cell)
+		}
 	}
 	close(cell.Notify)
 }
 
 func (program *Program) Run() {
-	log.Println(program.Input)
-	log.Println(program.Output)
+	if *debug {
+		log.Println(program.Input)
+		log.Println(program.Output)
+	}
 	for _, cell := range program.Cells {
-		log.Println(cell)
-		log.Println(cell.Type)
+		if *debug {
+			log.Println(cell)
+			log.Println(cell.Type)
+		}
 		go cell.Clock(program.Halt)
 	}
 }
--- src/cubeflow/main.go
+++ src/cubeflow/main.go
@@ -1,16 +1,69 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"io"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
+var inputData = flag.String("i", "", "input data")
+var debug = flag.Bool("d", false, "debug, mode")
+
 func main() {
-	tokenGrid, err := lexer(os.Stdin)
-	if err != nil {
-		os.Exit(1)
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
+		log.Fatal(flag.Args())
+		log.Fatal("a script is required")
 	}
-	fmt.Fprintln(os.Stderr, tokenGrid)
+
+	f, err := os.Open(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	tokenGrid, err := lexer(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	program := assembleLayer(tokenGrid)
-	fmt.Fprintln(os.Stderr, program)
+	if program == nil {
+		log.Fatal("compilation error")
+	} else if *debug {
+		log.Println(program)
+	}
+
+	go func() {
+		var r io.Reader
+		if *inputData != "" {
+			r = strings.NewReader(*inputData)
+		} else {
+			r = os.Stdin
+		}
+		scanner := bufio.NewScanner(r)
+		for scanner.Scan() {
+			n, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				log.Fatal(err)
+			}
+			program.Input <- Value(n)
+		}
+		program.Halt <- 0
+	}()
+
+	go func() {
+		for v := range program.Output {
+			fmt.Println(os.Stdout, v)
+		}
+	}()
+
+	program.Run()
+	<-program.Halt
 }
